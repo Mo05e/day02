c68d9ea6af8cdf20a306bcfd8af69de7bb132cfd
Muneer Miller <mumiller@c3r6s3.wethinkcode.co.za>
2020-06-19 12:18:37 +0200

    create ex00

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumiller <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 11:33:17 by mumiller          #+#    #+#             */
+/*   Updated: 2020/06/19 11:33:28 by mumiller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int ft_putchar(char n);
+void ft_print_alphabet(void);
+
+int main()
+{
+	
+	ft_print_alphabet();
+   
+    return 0;
+}
+
+int ft_putchar(char n)
+{
+	write(1,&n,1);
+	return 0;
+}
+void ft_print_alphabet(void)
+{
+	char c ='a';
+	while(c <= 'z')
+	{
+		ft_putchar(c++);
+	}
+	ft_putchar('\n');
+
+
+
+}
